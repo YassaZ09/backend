f6b97ec14032e5481a3c1369261dead09d370cd4
kochchicuddah <trailceylonlanka@gmail.com>
2025-01-03 23:29:41 +0530

    init commit

--- polkadanawa/src/main/java/com/example/polkadanawa/Controller/TranslationController.java
+++ polkadanawa/src/main/java/com/example/polkadanawa/Controller/TranslationController.java
@@ -7,6 +7,7 @@ import com.example.polkadanawa.Service.TranslationService;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/translations")
 public class TranslationController {
